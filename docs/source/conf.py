10b475e9e35eecbf96875dc6d2b0a2ab488dd085
ambitbuild <137879109+ambitbuild@users.noreply.github.com>
2023-06-27 11:46:33 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AmbitDocs'
+copyright = '2023, Ambit Team'
+author = 'Ambit Team'
 
 release = '0.1'
 version = '0.1.0'
